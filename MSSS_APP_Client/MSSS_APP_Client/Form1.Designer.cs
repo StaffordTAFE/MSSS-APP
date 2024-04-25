2d44855aaa2817a6a8bf8057ee5caf49e31f02f7
StaffordTAFE <140127581+StaffordTAFE@users.noreply.github.com>
2024-04-25 17:45:56 +0800

    Remake client app in .net 4.8 because Microsoft sucks

---
+++ MSSS_APP_Client/MSSS_APP_Client/Form1.Designer.cs
@@ -0,0 +1,460 @@
+﻿namespace MSSS_APP_Client
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.listView2 = new System.Windows.Forms.ListView();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.listView3 = new System.Windows.Forms.ListView();
+			this.label7 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.listView4 = new System.Windows.Forms.ListView();
+			this.label10 = new System.Windows.Forms.Label();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label11 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label12 = new System.Windows.Forms.Label();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.toolStrip1.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox4.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.flowLayoutPanel1.Controls.Add(this.groupBox1);
+			this.flowLayoutPanel1.Controls.Add(this.groupBox2);
+			this.flowLayoutPanel1.Controls.Add(this.groupBox3);
+			this.flowLayoutPanel1.Controls.Add(this.groupBox4);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 37);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(1165, 700);
+			this.flowLayoutPanel1.TabIndex = 0;
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripSeparator1,
+            this.toolStripDropDownButton1});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(1165, 38);
+			this.toolStrip1.TabIndex = 1;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripLabel1
+			// 
+			this.toolStripLabel1.Name = "toolStripLabel1";
+			this.toolStripLabel1.Size = new System.Drawing.Size(258, 33);
+			this.toolStripLabel1.Text = "MSSS Astronomical Processing";
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 38);
+			// 
+			// toolStripDropDownButton1
+			// 
+			this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+			this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+			this.toolStripDropDownButton1.Size = new System.Drawing.Size(120, 33);
+			this.toolStripDropDownButton1.Text = "Preferences";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.listView1);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.textBox2);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(3, 3);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(283, 688);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Star Velocity";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(228, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Observed Wavelength (Metres)";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(9, 45);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(268, 26);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(9, 97);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(268, 26);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 74);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(194, 20);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Rest Wavelength (Metres)";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(9, 129);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(268, 30);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Calculate";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(9, 162);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(267, 20);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Calculation Results (Metres/Second)";
+			// 
+			// listView1
+			// 
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(13, 185);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(264, 497);
+			this.listView1.TabIndex = 6;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.listView2);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.button2);
+			this.groupBox2.Controls.Add(this.label5);
+			this.groupBox2.Controls.Add(this.textBox4);
+			this.groupBox2.Controls.Add(this.label6);
+			this.groupBox2.Location = new System.Drawing.Point(292, 3);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(283, 688);
+			this.groupBox2.TabIndex = 7;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Star Distance";
+			// 
+			// listView2
+			// 
+			this.listView2.HideSelection = false;
+			this.listView2.Location = new System.Drawing.Point(13, 133);
+			this.listView2.Name = "listView2";
+			this.listView2.Size = new System.Drawing.Size(264, 549);
+			this.listView2.TabIndex = 6;
+			this.listView2.UseCompatibleStateImageBehavior = false;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(9, 110);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(216, 20);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Calculation Results (Parsecs)";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(9, 77);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(268, 30);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Calculate";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(9, 74);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(0, 20);
+			this.label5.TabIndex = 2;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(9, 45);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(268, 26);
+			this.textBox4.TabIndex = 1;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(9, 22);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(236, 20);
+			this.label6.TabIndex = 0;
+			this.label6.Text = "Arcseconds Angle (Arcseconds)";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.listView3);
+			this.groupBox3.Controls.Add(this.label7);
+			this.groupBox3.Controls.Add(this.button3);
+			this.groupBox3.Controls.Add(this.label8);
+			this.groupBox3.Controls.Add(this.textBox3);
+			this.groupBox3.Controls.Add(this.label9);
+			this.groupBox3.Location = new System.Drawing.Point(581, 3);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(283, 688);
+			this.groupBox3.TabIndex = 8;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Celsius to Kelvin";
+			// 
+			// listView3
+			// 
+			this.listView3.HideSelection = false;
+			this.listView3.Location = new System.Drawing.Point(13, 133);
+			this.listView3.Name = "listView3";
+			this.listView3.Size = new System.Drawing.Size(264, 549);
+			this.listView3.TabIndex = 6;
+			this.listView3.UseCompatibleStateImageBehavior = false;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(9, 110);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(200, 20);
+			this.label7.TabIndex = 5;
+			this.label7.Text = "Calculation Results (Kelvin)";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(9, 77);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(268, 30);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Calculate";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(9, 74);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(0, 20);
+			this.label8.TabIndex = 2;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(9, 45);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(268, 26);
+			this.textBox3.TabIndex = 1;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(9, 22);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(165, 20);
+			this.label9.TabIndex = 0;
+			this.label9.Text = "Temperature (Celsius)";
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Controls.Add(this.listView4);
+			this.groupBox4.Controls.Add(this.label10);
+			this.groupBox4.Controls.Add(this.button4);
+			this.groupBox4.Controls.Add(this.label11);
+			this.groupBox4.Controls.Add(this.textBox5);
+			this.groupBox4.Controls.Add(this.label12);
+			this.groupBox4.Location = new System.Drawing.Point(870, 3);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(283, 688);
+			this.groupBox4.TabIndex = 8;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "Schwarzschild Radius ";
+			// 
+			// listView4
+			// 
+			this.listView4.HideSelection = false;
+			this.listView4.Location = new System.Drawing.Point(13, 133);
+			this.listView4.Name = "listView4";
+			this.listView4.Size = new System.Drawing.Size(264, 549);
+			this.listView4.TabIndex = 6;
+			this.listView4.UseCompatibleStateImageBehavior = false;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(9, 110);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(208, 20);
+			this.label10.TabIndex = 5;
+			this.label10.Text = "Calculation Results (Metres)";
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(9, 77);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(268, 30);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "Calculate";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(9, 74);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(0, 20);
+			this.label11.TabIndex = 2;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(9, 45);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(268, 26);
+			this.textBox5.TabIndex = 1;
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(9, 22);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(210, 20);
+			this.label12.TabIndex = 0;
+			this.label12.Text = "Black Hole Mass (Kilograms)";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1165, 740);
+			this.Controls.Add(this.toolStrip1);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.Text = "MSSS Astronomical Processing Client";
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox4.ResumeLayout(false);
+			this.groupBox4.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.ListView listView2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.ListView listView3;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.ListView listView4;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label12;
+	}
+}
+
---
+++ MSSS_APP_Client/MSSS_APP_Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using MSSS_Astronomical_Processing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSSS_APP_Client
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+
+			Console.WriteLine("Client Started");
+			ChannelFactory<IAstroContract> pipeFactory =
+			new ChannelFactory<IAstroContract>(
+			new NetNamedPipeBinding(),
+			new EndpointAddress("net.pipe://localhost/PipeReverse"));
+			IAstroContract pipeProxy = pipeFactory.CreateChannel();
+		}
+	}
+}
